874c3df2f1e42e0f0ed97d2beb2977b38a7cd4cc
rajibhossainimran <rajibhossainimran@gmail.com>
2025-02-07 23:57:19 +0600

    update

--- uk-propcert-frontend/src/Pages/FAQ/FAQ.jsx
+++ uk-propcert-frontend/src/Pages/FAQ/FAQ.jsx
@@ -5,20 +5,16 @@ const FAQ = () => {
     return (
         <div>
             <div className="relative w-full h-full top-14">
-                {/* Background Image */}
                 <img
                     src="https://cdn-gomlann.nitrocdn.com/wpmcSMBdIEhoNpEMKfsZwxDMFqniqTMp/assets/images/optimized/rev-119317a/www.propcert.co.uk/wp-content/uploads/2024/01/FAQ_2-scaled.jpg"
                     alt="Overlay Image"
 
                     className=' '
-
-                // className="w-full h-[600px]  "
                 />
 
-                {/* Overlay */}
                 <div className="absolute inset-0 bg-black opacity-20"></div>
 
-                {/* Content */}
+           
                 <div className="absolute inset-0 flex flex-col items-start justify-center  text-white  px-4 text-center leading-relaxed">
                     <h1 className="text-5xl md:text-[70px] font-bold mb-4" style={{ textShadow: "2px 2px 5px rgba(0,0,0,0.7)" }}
                     >Frequently <br /> asked questions</h1>
